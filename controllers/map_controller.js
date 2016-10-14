e3809ceb62157617cca34ec21b2b55cab7a467c6
joe <joeida@jpl.nasa.gov>
2016-10-14 11:53:43 -0700

    Added map controller and linked to safezone API module

---
+++ controllers/map_controller.js
@@ -0,0 +1,32 @@
+const models = require('../models');
+const express = require('express');
+const router = express.Router();
+const safezone = require('../js/safezone.js');
+
+router.get('/', function(req, res) {
+    const addressObj = safezone.getAddressObj('pasadena', 'ca');
+    safezone.getGeoObj(addressObj, function(geoObj) {
+        safezone.getSafezoneList(geoObj, function(safezoneResultList) {
+            console.log(safezoneResultList);
+        });
+    });
+
+    models.Data.findAll({
+            include: [models.User]
+        })
+        // connect the findAll to this .then
+        .then(function(data) {
+            // grab the user info from our req.
+            // How is it in our req?
+            // This info gets saved to req via the users_controller.js file.
+            res.render('index', {
+                user_id: req.session.user_id,
+                username: req.session.user_name,
+                email: req.session.user_email,
+                logged_in: req.session.logged_in,
+                data: data
+            });
+        });
+});
+
+module.exports = router;
