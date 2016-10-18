e3088e660c93dfa7ef63fc9a043db96ebcf38cf3
neurohacked <admin@neurohacked.com>
2016-10-17 17:02:25 -0700

    Location addition changes

--- js/safezone.js
+++ js/safezone.js
@@ -18,7 +18,7 @@ module.exports = {
                 var addressHash = city.split(" ").join("") + state.split(" ").join("");
             }
             var addressObj = {
-                choiceHash: addressHash, 
+                choiceHash: addressHash,
                 addressList: addressList,
                 city: city,
                 state: state
@@ -28,7 +28,7 @@ module.exports = {
     },
 
     // Using address and search criteria, use Google Geo API to get Latitude and Longitude of location chosen and return callback with result to get processed
-    getGeoObj: function (addressObj, cb) {
+    getGeoObj: function(addressObj, cb) {
         var query = '';
         var queryList = [];
         var addressList = addressObj.addressList;
@@ -47,13 +47,13 @@ module.exports = {
         }
         query = queryList.join(',+');
         var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&key=AIzaSyBZgPjyk5ho6Axhr_2dU1Ay3M7rU71HXvs";
-        request(queryURL, function (error, response, body) {
+        request(queryURL, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var returned = JSON.parse(body).results[0];
                 var geoObj = {
                     lat: returned.geometry.location.lat,
                     lng: returned.geometry.location.lng,
-                    address: returned.formatted_address, 
+                    address: returned.formatted_address,
                     choiceHash: addressObj.choiceHash
                 };
 
@@ -66,7 +66,7 @@ module.exports = {
     },
 
     // Get safezone locations list based on current geoCoordinates and a location type specified by function caller and return callback with result to get processed
-    getSafezoneList: function (geoObj, cb) {
+    getSafezoneList: function(geoObj, cb) {
         var locationTypeList = ['embassy', 'hospital', 'police', 'airport', 'bank'];
         var safezoneCounter = 0;
         var safezoneResultList = [];
@@ -83,8 +83,8 @@ module.exports = {
 
         locationTypeList.forEach(function(locationType) {
 
-            var queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + geoObj.lat +',' + geoObj.lng + '&radius=5000&types=' + locationType + '&key=AIzaSyCdKTEHizAqhcNWoqo7TjU3WN0E4miTwBc'
-            request(queryURL, function (error, response, body) {
+            var queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + geoObj.lat + ',' + geoObj.lng + '&radius=5000&types=' + locationType + '&key=AIzaSyCdKTEHizAqhcNWoqo7TjU3WN0E4miTwBc'
+            request(queryURL, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
                     var returned = JSON.parse(body).results;
                     returned.forEach(function(child) {
