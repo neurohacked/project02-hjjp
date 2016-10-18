24d67742bf0b7fe5c84a4a8aeaced8ffada901de
neurohacked <admin@neurohacked.com>
2016-10-18 13:24:31 -0700

    Fix map controller for merge

--- controllers/map_controller.js
+++ controllers/map_controller.js
@@ -1,24 +1,21 @@
 var models = require('../models');
 var express = require('express');
-var safezones = require('../js/safezone.js');
-// var weather = require('../js/weather.js');
 var router = express.Router();
+var safezone = require('../js/safezone.js');
 
-// // get map input page
-// router.get('/', function(req, res) {
-//     res.render('map');
-// });
+// get map input page
+router.get('/', function(req, res) {
+    res.render('map');
+});
 
 // get safezones
-router.post('/safezones', function(req, res) {
-    var addressObj = safezones.getAddressObj(req.body.address, req.body.city, req.body.state);
-    safezones.getGeoObj(addressObj, function(geoObj) {
-        safezones.getSafezonesList(geoObj, function(safezonesResultList) {
-            res.send(safezonesResultList);
+router.post('/search', function(req, res) {
+    var addressObj = safezone.getAddressObj(req.body.address, req.body.city, req.body.state);
+    safezone.getGeoObj(addressObj, function(geoObj) {
+        safezone.getSafezoneList(geoObj, function(safezoneResultList) {
+            res.send(safezoneResultList);
         });
     });
 });
-
-
 
 module.exports = router;
