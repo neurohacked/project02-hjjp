df230fec8f881bccd6ecd01876123fe6113b3ed8
Gary Jackemuk <jackemuk@jackemuk.com>
2016-10-18 20:53:48 -0700

    Added basic response for risk factor

--- controllers/risk_controller.js
+++ controllers/risk_controller.js
@@ -2,8 +2,17 @@ const models = require('../models');
 const express = require('express');
 const router = express.Router();
 
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 router.get('/risk', function(req, res) {
-    console.log("Inside /risk")    
+    console.log("Inside /risk");
+    res.send(getRandomIntInclusive(0,100));
 });
 
 
