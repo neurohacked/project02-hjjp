d3b8405fcb8ed3cca56e7e910acde11510b24461
neurohacked <admin@neurohacked.com>
2016-10-19 09:42:02 -0700

    Export handlebars helpers.

---
+++ js/hbsHelpers.js
@@ -0,0 +1,29 @@
+const hbs = require('handlebars');
+
+module.exports = {
+
+    // box color based on risk
+    boxColor: hbs.registerHelper("box-color", function(risk) {
+        if (risk >= 80) {
+            return "box-danger";
+        } else if (risk >= 60) {
+            return "box-warning";
+        } else if (risk >= 40) {
+            return "box-default";
+        } else {
+            return "box-success";
+        }
+    }),
+    // btn color based on risk
+    btnColor: hbs.registerHelper("btn-color", function(risk) {
+        if (risk >= 80) {
+            return "bg-red";
+        } else if (risk >= 60) {
+            return "bg-orange";
+        } else if (risk >= 40) {
+            return "bg-gray";
+        } else {
+            return "bg-green";
+        }
+    })
+}
