c929f09e518397737c99f26f72a94fda6a0ed87f
Hannah Marlin <hmmarlin@gmail.com>
2016-10-19 09:37:36 -0700

    add new weather api darksky

--- js/weather.js
+++ js/weather.js
@@ -1,36 +1,15 @@
-var weather = require('weather-js');
+    var apiKey = '63ff12333f5125605590036f6f111772';
+    var lat = '17.3817219'
+    var lng = '121.5417694'
+    var queryURL = "https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + lng;
 
-    var city = process.argv[2];
-
-    weather.find({
-        search: city,
-        degreeType: "F"
-    }, function(err, result) {
-
-        if (err) {
-            console.log(err);
-        }
-            console.log('YESTERDAY: ' + JSON.stringify(result[0].forecast[0].day))
-            console.log('---------------------------')
-            console.log(JSON.stringify(result[0].forecast[0], null, 6)); //yesterday full forecast
-            console.log(JSON.stringify(result[0].forecast[0].low, null, 6)); //yesterday lowest temp
-            console.log(JSON.stringify(result[0].forecast[0].high, null, 6)); //yesterday highest temp
-
-            console.log('TODAY: ' + JSON.stringify(result[0].forecast[1].day))
-            console.log('---------------------------')
-            console.log(JSON.stringify(result[0].forecast[1], null, 6)); //today full forecast
-            console.log(JSON.stringify(result[0].forecast[1].low, null, 6)); //today lowest temp
-            console.log(JSON.stringify(result[0].forecast[1].high, null, 6)); //today highest temp
-
-            console.log('TOMORROW: ' + JSON.stringify(result[0].forecast[2].day))
-            console.log('---------------------------')
-            console.log(JSON.stringify(result[0].forecast[2], null, 6)); //tomorrow full forecast
-            console.log(JSON.stringify(result[0].forecast[2].low, null, 6)); //tomorrow lowest temp
-            console.log(JSON.stringify(result[0].forecast[2].high, null, 6)); //tomorrow highest temp
-
-            // if we want to serve just current conditions:
-
-            console.log(JSON.stringify(result[0].current, null, 6)); //right now full conditions
-            console.log(JSON.stringify(result[0].current.imageUrl, null, 6)); //right now icon
-
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+        dataType: 'jsonp'
+    }).done(function(response) {
+        console.log(response); // full response obj
+        console.log(response.currently.temperature); // current temp
+        console.log(response.daily.data[0].summary); // current description
+        
     });
