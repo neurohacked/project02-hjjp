792355758696ac1b9b1ebfd7f197fab2ce17920c
neurohacked <admin@neurohacked.com>
2016-10-21 09:07:27 -0700

    Fix for merge

--- js/safezoneCount.js
+++ js/safezoneCount.js
@@ -1,14 +1,16 @@
-const safezones = require('../js/safezone');
+safezones = require('safezone.js');
 
 module.exports = {
 
     // Gets number of safezones for current search to be used for threat calculation.
-    getSafezoneCount: function(address, cb) {
-        var addressObj = safezones.getAddressObj(address);
-        safezones.getGeoObj(addressObj, function(geoObj) {
-            safezones.getSafezonesList(geoObj, function(safezonesResultList) {
-                cb(safezonesResultList);
-            });
+    getSafezoneCount: function(address, lat, lng, cb) {
+        var locationObj = {
+            address: address,
+            lat: parseFloat(lat),
+            lng: parseFloat(lng)
+        }
+        safezones.getSafezonesList(locationObj, function(safezonesResultList) {
+            cb(safezonesResultList.length);
         });
     }
 }
