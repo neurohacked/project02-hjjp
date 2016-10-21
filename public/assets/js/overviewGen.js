7dad4956b48d1485d36912d4d52527d970fe1a6e
neurohacked <admin@neurohacked.com>
2016-10-21 14:54:51 -0700

    Cleanup weather and overview modals.

--- public/assets/js/overviewGen.js
+++ public/assets/js/overviewGen.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
         $('.overview-collection').empty();
         var modalHtml = '<div class="modal fade overview-modal" tabindex="-1" role="dialog" aria-labelledby="overview-modal">'+
-            '<div class="modal-dialog modal-md" role="document">'+
+            '<div class="modal-dialog modal-lg" role="document">'+
                 '<!-- Modal content-->'+
                 '<div class="modal-content">'+
                     '<div class="box-header with-border">'+
@@ -12,7 +12,18 @@ $(document).ready(function() {
                         '<h4 class="box-title">Risk Overview</h4>'+
                     '</div>'+
                     '<div class="box-body">'+
-                        '<div id="risk-data">'+
+                        '<div class="row">'+
+                            '<div class="col-lg-12">'+
+                                '<div class="col-lg-4">'+
+                                    '<div class="well info-box">'+
+                                        '<span></span>' +
+                                    '</div>'+
+                                '</div>'+
+                                '<div class="col-lg-8">'+
+                                    '<div id="risk-data">'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
                         '</div>'+
                     '</div>'+
                     '<div class="box-footer no-padding text-center">'+
@@ -24,6 +35,13 @@ $(document).ready(function() {
 
         $('.overview-modal').modal('show');
         $('.overview-modal').on('shown.bs.modal', function () {
+            var riskFactors = {
+                crime: 25,
+                weather: 40,
+                safehouses: 25,
+                terror: 10
+            }
+
             Morris.Donut({
                 element: 'risk-data',
                 data: [
